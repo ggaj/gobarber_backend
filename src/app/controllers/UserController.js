02782bce74ea58c4dc6d16a75d04dc5cc3bfbbbd
Gildo Jr <gildojr@MacBook-Air-de-Gildo.local>
2019-12-04 19:51:41 -0400

    initial commit

---
+++ src/app/controllers/UserController.js
@@ -0,0 +1,86 @@
+import * as Yup from 'yup';
+import User from '../models/User';
+import File from '../models/File';
+
+class UserController {
+  async store(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      email: Yup.string().email().required(),
+      password: Yup.string().required().min(6),
+    });
+
+    if (!(schema.isValid(req.body))) {
+      return res.status(400).json({error: 'Validations fails'});
+    }
+
+    const userExists = await User.findOne({where: {email: req.body.email}});
+    if (userExists) {
+      return res.status(400).json({error: 'User alredy exists.'});
+    }
+    const {id, name, email, provider} = await User.create(req.body);
+    return res.json({
+      id,
+      name,
+      email,
+      provider,
+    });
+  }
+
+  async update(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string(),
+      email: Yup.string().email(),
+      oldPassword: Yup.string().min(6),
+      password: Yup.string().min(6).when('oldPassword', (oldPassword, field) =>
+        oldPassword ? field.required() : field
+      ),
+      confirmPassword: Yup.string().when('password', (password, field) =>
+        password ? field.required().oneOf([Yup.ref('password')]) : field
+      ),
+    });
+
+    if (!(schema.isValid(req.body))) {
+      return res.status(400).json({error: 'Validations fails'});
+    }
+
+    const {email, oldPassword} = req.body;
+
+    const user = await User.findByPk(req.userId);
+
+    if (user) {
+      if (email !== user.email) {
+        const userExists = await User.findOne({where: {email: req.body.email}});
+        if (userExists) {
+          return res.status(400).json({error: 'User alredy exists.'});
+        }
+      }
+
+      if (oldPassword && !(await user.checkPassword(oldPassword))) {
+        return res.status(401).json({error: 'Password does not match'});
+      }
+
+      await user.update(req.body);
+
+      const {id, name, provider} = await User.findByPk({
+        id: req.userId,
+        include: [{
+          model: File,
+          as: 'avatar',
+          attributes: ['id', 'path', 'url'],
+        }],
+      });
+
+      return res.json({
+        id,
+        name,
+        email,
+        provider,
+      });
+    } else {
+      return res.status(404).json({error: 'User not found'});
+    }
+  }
+}
+
+export default new UserController();
